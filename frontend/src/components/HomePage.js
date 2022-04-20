42b4f01e9a69fb1e1b53112944cd50ba01ce0559
William Misiaszek <48428545+William-Misiaszek@users.noreply.github.com>
2022-04-19 22:11:10 -0700

    Stanford Name

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -36,7 +36,7 @@ export default class HomePage extends Component {
       <Grid container spacing={3}>
         <Grid item xs={12} align="center">
           <Typography variant="h3" compact="h3">
-            Groovii
+            Stanford Jam App
           </Typography>
         </Grid>
         <Grid item xs={12} align="center">
